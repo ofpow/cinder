7f8742cb6c0a7f8540c867e59b213c6935d6ab98
Oliver Powell <oliver@drpowell.org>
2025-12-25 13:42:51 -0500

    Add rays

--- main.c
+++ main.c
@@ -1,24 +1,49 @@
 #include <stdio.h>
 
 #include "vec3.h"
+#include "ray.h"
 
 #define X 200
 #define Y 100
+
+vec3 color(Ray r) {
+    vec3 unit_direction = unit_vector(r.direction);
+    float t = 0.5 * (unit_direction.y + 1.0);
+    return add_vec3(
+        scale_vec3((vec3){1.0, 1.0, 1.0}, (1.0-t)),
+        scale_vec3((vec3){0.5, 0.7, 1.0}, t)
+    );
+}
 
 int main(void) {
     printf("P3\n");
     printf("%d %d\n", X, Y);
     printf("255\n");
 
+    vec3 lower_left_corner = {-2.0, -1.0, -1.0};
+    vec3 horizontal = {4.0, 0.0, 0.0};
+    vec3 vertical = {0.0, 2.0, 0.0};
+    vec3 origin = {0.0, 0.0, 0.0};
+
     for (int j = Y - 1; j >= 0; j--) {
         for (int i = 0; i < X; i++) {
-            vec3 v = {
-                (float)i / (float)X,
-                (float)j / (float)Y,
-                0.2
+            float u = (float)i / (float)X;
+            float v = (float)j / (float)Y;
+
+            Ray r = {
+                origin,
+                add_vec3(
+                    lower_left_corner,
+                    add_vec3(
+                        scale_vec3(horizontal, u),
+                        scale_vec3(vertical, v)
+                    )
+                )
             };
 
-            printf("%d %d %d\n", (int)(255.99*v.x), (int)(255.99*v.y), (int)(255.99*v.z));
+            vec3 col = color(r);
+
+            printf("%d %d %d\n", (int)(255.99*col.x), (int)(255.99*col.y), (int)(255.99*col.z));
         }
     }
     
--- vec3.h
+++ vec3.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <math.h>
+
 typedef struct vec3 {
     float x;
     float y;
@@ -8,4 +10,24 @@ typedef struct vec3 {
 
 void print_vec3(vec3 v) {
     printf("{%f, %f, %f}\n", v.x, v.y, v.z);
+}
+
+vec3 add_vec3(vec3 a, vec3 b) {
+    return (vec3){
+        a.x + b.x,
+        a.y + b.y,
+        a.z + b.z
+    };
+}
+
+vec3 scale_vec3(vec3 v, float s) {
+    return (vec3){v.x * s, v.y * s, v.z * s};
+}
+
+float vec3_length(vec3 v) {
+    return sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
+vec3 unit_vector(vec3 v) {
+    return scale_vec3(v, 1 / vec3_length(v));
 }

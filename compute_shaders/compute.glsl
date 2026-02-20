1f49151cb206152e6c5786b29de032ed5af189ee
Oliver Powell <oliver@drpowell.org>
2026-02-20 13:04:47 -0500

    Background

--- compute_shaders/compute.glsl
+++ compute_shaders/compute.glsl
@@ -9,8 +9,31 @@ layout(std430, binding = 1) restrict buffer buf_layout {
 uniform int X;
 uniform int Y;
 
+struct Ray {
+    vec3 origin;
+    vec3 direction;
+};
+
+vec3 unit_vector(vec3 v) {
+    return v / length(v);
+}
+
+vec3 color(Ray r) {
+    vec3 unit_dir = unit_vector(r.direction);
+    float t = 0.5*(-unit_dir.y + 1.0);
+    return (1.0-t)*vec3(1.0) + t*vec3(0.5, 0.7, 1.0);
+}
+
+vec3 lower_left_corner = vec3(-2.0, -1.0, -1.0);
+vec3 horizontal = vec3(4.0, 0.0, 0.0);
+vec3 vertical = vec3(0.0, 2.0, 0.0);
+vec3 origin = vec3(0.0, 0.0, 0.0);
+
 void main() {
     uint x = gl_GlobalInvocationID.x;
     uint y = gl_GlobalInvocationID.y;
-    buf[x + y*X] = vec4(float(x) / X, float(y) / Y, 0.2, 1.0);
+    float u = float(x) / X;
+    float v = float(y) / Y;
+    Ray r = Ray(origin, lower_left_corner + u*horizontal + v*vertical);
+    buf[x + y*X] = vec4(color(r), 1.0);
 }

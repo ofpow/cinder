da3e045fbd7c71ed422110cdb6e4b35ff4c49590
Oliver Powell <oliver@drpowell.org>
2026-02-20 18:49:11 -0500

    Sphere normals

--- compute_shaders/compute.glsl
+++ compute_shaders/compute.glsl
@@ -18,21 +18,29 @@ vec3 unit_vector(vec3 v) {
     return v / length(v);
 }
 
-bool hit_sphere(vec3 center, float radius, Ray r) {
+vec3 point_at_parameter(Ray r, float t) {
+    return r.origin + t*r.direction;
+}
+
+float hit_sphere(vec3 center, float radius, Ray r) {
     vec3 oc = r.origin - center;
     float a = dot(r.direction, r.direction);
     float b = 2.0*dot(oc, r.direction);
     float c = dot(oc, oc) - radius*radius;
     float discriminant = b*b - 4*a*c;
-    return (discriminant > 0);
-    return false;
+    if (discriminant < 0)
+        return -1.0;
+    return (-b - sqrt(discriminant)) / (2.0*a);
 }
 
 vec3 color(Ray r) {
-    if (hit_sphere(vec3(0, 0, -1), 0.5, r))
-        return vec3(1, 0, 0);
+    float t = hit_sphere(vec3(0, 0, -1), 0.5, r);
+    if (t > 0.0) {
+        vec3 n = unit_vector(point_at_parameter(r, t) - vec3(0, 0, -1));
+        return 0.5*vec3(n.x + 1, -n.y + 1, n.z + 1);
+    }
     vec3 unit_dir = unit_vector(r.direction);
-    float t = 0.5*(-unit_dir.y + 1.0);
+    t = 0.5*(-unit_dir.y + 1.0);
     return (1.0-t)*vec3(1.0) + t*vec3(0.5, 0.7, 1.0);
 }
 

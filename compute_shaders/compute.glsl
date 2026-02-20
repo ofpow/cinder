50fb7d280caae7af514873fe7cc3b5f117a2917f
Oliver Powell <oliver@drpowell.org>
2026-02-20 15:18:53 -0500

    Sphere

--- compute_shaders/compute.glsl
+++ compute_shaders/compute.glsl
@@ -18,7 +18,19 @@ vec3 unit_vector(vec3 v) {
     return v / length(v);
 }
 
+bool hit_sphere(vec3 center, float radius, Ray r) {
+    vec3 oc = r.origin - center;
+    float a = dot(r.direction, r.direction);
+    float b = 2.0*dot(oc, r.direction);
+    float c = dot(oc, oc) - radius*radius;
+    float discriminant = b*b - 4*a*c;
+    return (discriminant > 0);
+    return false;
+}
+
 vec3 color(Ray r) {
+    if (hit_sphere(vec3(0, 0, -1), 0.5, r))
+        return vec3(1, 0, 0);
     vec3 unit_dir = unit_vector(r.direction);
     float t = 0.5*(-unit_dir.y + 1.0);
     return (1.0-t)*vec3(1.0) + t*vec3(0.5, 0.7, 1.0);
